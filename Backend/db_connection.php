a8029074c78ccb8cad327e1daa16ece4041e5b5b
Chadangdang <chadang.phu@gmail.com>
2025-05-01 15:37:05 +0700

    new deployment

--- Backend/db_connection.php
+++ Backend/db_connection.php
@@ -1,11 +1,12 @@
 <?php
-$servername = "localhost";
-$username = "root"; // Adjust if needed
-$password = "root"; // Adjust for your setup
-$dbname = "DB1"; // Your database name
+$servername = "sql110.infinityfree.com";  
+$username = "if0_38876323";               
+$password = "Chk2oo4o1";                 
+$dbname = "if0_38876323_db1";             
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
